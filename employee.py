cc1fb3bf89550ef6db28a0c017e144d888c523bd
Mayss Aldawsari <mayss.aldawsari@gmail.com>
2023-10-11 13:17:17 +0100

    Add the dict to each employee

--- employee.py
+++ employee.py
@@ -103,19 +103,19 @@ class Employee:
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = Employee('Billie', {'monthly':4000})
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+charlie = Employee('Charlie', {'hourly':100, 'price':25})
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+renee = Employee('Renee', {'monthly':3000, 'contracts': 4, 'commprice': 200})
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+jan = Employee('Jan', {'hourly':150, 'price':25, 'contracts': 3, 'commprice': 220})
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = Employee('Robbie', {'monthly':2000, 'bonus': 1500})
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+ariel = Employee('Ariel', {'hourly':120, 'price':30, 'bonus': 600})
